2cb4a169792cacf7aeeb57fc516c5a20a55cdae8
ekta <ekta@myphysicianplan.com>
2022-12-06 17:32:51 -0500

    cc

--- Scripts/Common.js
+++ Scripts/Common.js
@@ -1,4 +1,15 @@
-﻿function setJsonParameter(parameterName, parameterValue, methodName) {
+﻿function getDate(str) { var ops = { year: 'numeric' }; ops.month = ops.day = '2-digit'; return new Date(str).toLocaleDateString(0, ops); }
+
+function getFormattedDate(date) {
+    date = new Date(date);
+    var year = date.getFullYear();
+    var month = (1 + date.getMonth()).toString().padStart(2, '0');
+    var day = date.getDate().toString().padStart(2, '0');
+
+    return month + '/' + day + '/' + year;
+}
+
+function setJsonParameter(parameterName, parameterValue, methodName) {
     var obj = new Object();
     obj.ParameterName = parameterName;
     obj.ParameterValue = parameterValue;
@@ -11,6 +22,14 @@
 function getformattedJsonFromArray(arrayObj) {
     arrayObj = arrayObj.replace(/"/g, "'");
     return arrayObj + "";
+}
+
+function setParameter(parameterName, methodName) {
+    var obj = new Object();
+    obj.WebMethodName = methodName;
+    obj.XMLdata = parameterName;
+    var resultData = JSON.stringify(obj);
+    return resultData;
 }
 
 function BindDropdown(LookupTypeId, dropdownName, Url) {
@@ -148,10 +167,3 @@ function BindOrganizations(autoCompleteName, url) {
     });
 }
 
-function setParameter(parameterName, methodName) {
-    var obj = new Object();
-    obj.WebMethodName = methodName;
-    obj.XMLdata = parameterName;
-    var resultData = JSON.stringify(obj);
-    return resultData;
-}
